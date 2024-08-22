892a7e820225f4a012c956fad18930d357a6a403
Olivier Barais <olivieR.barais@gmail.com>
2024-08-22 18:26:29 +0200

    update

--- docs/assets/slidev/DrawingPreview-BKHlwOe5.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-BPQmc3Na.js";import"../modules/vue-DCbP7Xfw.js";import"../index-D6wL_I3j.js";import"../modules/shiki-Dv8q3HIQ.js";export{o as default};
